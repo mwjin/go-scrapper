3bfebcd6ff7a5eb392af1bfda3d53a31c4b8ee9a
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-06-10 23:52:24 +0900

    Extract id, title, and summary from a job card

--- main.go
+++ main.go
@@ -4,19 +4,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseUrl string = "https://kr.indeed.com/jobs?q=python&l="
+type job struct {
+	id       string
+	location string
+	title    string
+	salary   string
+	summary  string
+}
+
+var baseUrl string = "https://kr.indeed.com/jobs?q=python"
 
 func main() {
-	totalPages := getPages()
+	totalPages := GetPageCnt()
 	fmt.Println(totalPages)
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageUrl := baseUrl + "&start=" + strconv.Itoa(page*10)
+	fmt.Println("Requesting " + pageUrl)
+	res, err := http.Get(pageUrl)
+	checkErr(err)
+	checkStatus(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find(".title>a").Text()
+		location := card.Find(".sjcl").Text()
+		fmt.Println(id, title, location)
+	})
 }
 
 // How many pages are there?
-func getPages() int {
+func GetPageCnt() int {
 	pageCnt := 0
 	res, err := http.Get(baseUrl)
 	checkErr(err)
