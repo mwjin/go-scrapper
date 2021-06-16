c595d17b1a7cd8519b5ca76535fdf3944c539d33
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-06-16 22:07:11 +0900

    Implement a web server to scrape jobs

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo"
+	"github.com/mwjjeong/go-scrapper/scrapper"
 )
+
+const fileName string = "jobs.csv"
 
 func main() {
 	e := echo.New()
 	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrapper)
 	e.Logger.Fatal(e.Start(":8700"))
 }
 
 func handleHome(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.File("home.html")
+}
+
+func handleScrapper(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	fmt.Println(term)
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
 }
